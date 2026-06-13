9ae211e4e9a4fdb15470c3a22a2be253963e2f4f
Sisriksha Dhakal <binitadeuja66@gmail.com>
2026-06-13 08:53:43 -0700

    updated requirement

--- test.py
+++ test.py
@@ -9,4 +9,5 @@ if model:
     st.success('Model loaded successfully!')
 else:
     st.error('Failed to load the model.')
-print(sklearn.__version__)
+st.success(f'Scikit-learn version: {sklearn.__version__}')
+st.success(f'Python version: {os.getenv("PYTHON_VERSION")}')
